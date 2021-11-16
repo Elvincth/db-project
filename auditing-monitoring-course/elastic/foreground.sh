2e4d5494a5af34e1df9b7ee908d683fc10e0027d
Elvin Chu <elvincth@gmail.com>
2021-11-16 12:48:41 +0800

    fix

--- auditing-monitoring-course/elastic/foreground.sh
+++ auditing-monitoring-course/elastic/foreground.sh
@@ -1,8 +1,14 @@
-git clone https://github.com/Elvincth/docker-EFK.git
-cd docker-EFK
+mkdir tutorial
+cd tutorial
 mkdir db_data
 mkdir wordpress_data
 mkdir es_data
 mkdir apache_logs
+touch docker-compose.yml
+touch elasticsearch.yml
+touch filebeat_apache.yml
+touch filebeat_db.yml
+touch init.sql
+touch kibana.yml
+touch my.cnf
 sudo chmod -R 777 .
-docker-compose up -d
---
+++ auditing-monitoring-course/elastic/step1.md
@@ -0,0 +1,3 @@
+### Explore the docker compose stack
+
+Beforehand, we have already help you setup all the configuration files and docker-compose.yml we will 
